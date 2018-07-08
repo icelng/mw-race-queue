d62c181fac40a7625e91296789e2fe51e0579801
yiran_fancier <ip28@qq.com>
2018-07-08 16:37:12 +0800

    o_direct,写成功,本地非常快! buffers 2200000

--- queue_store.cpp
+++ queue_store.cpp
@@ -26,7 +26,7 @@ queue_store::queue_store() {
 //    store_io = new StoreIO("./log", FILE_SIZE, REGION_SIZE);
     idle_page_manager = new IdlePageManager(FILE_SIZE, 4096);
 //    buffer_pool = new BufferPool(16000000, 256);
-    buffer_pool = new BufferPool(2800000, 2048);
+    buffer_pool = new BufferPool(2200000, 2048);
 //    buffer_pool = new BufferPool(8000000, 512);
     commit_service = new CommitService(store_io, 1);
     commit_service->start();
